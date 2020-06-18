41995f072ee05016fc22d2a5219f999a46c21fd1
Yusuke Taira <ney07214545@icloud.com>
2020-06-18 18:49:43 +0900

    Update error response in SignUp function

--- user-rest-service/main.go
+++ user-rest-service/main.go
@@ -81,18 +81,18 @@ func UserValidate(user *User) *ErrorMsg {
 	return nil
 }
 
-func checkForUniqueID(h *sqlHandler, user *User) *ErrorMsg {
+func checkForUniqueID(h *sqlHandler, user *User) (*ErrorMsg, error) {
 	var errorMsg ErrorMsg
 	var dbID string
 	if err := h.db.QueryRowx("SELECT id FROM users WHERE id = ?", user.ID).Scan(&dbID); err != nil {
 		if err == sql.ErrNoRows {
-			return nil
+			return nil, nil
 		} else if err != nil {
-			log.Printf("failed to scan the row: %v", err)
+			return nil, err
 		}
 	}
 	errorMsg.ID = "このユーザーIDは登録できません"
-	return &errorMsg
+	return &errorMsg, nil
 }
 
 func responseByJson(w http.ResponseWriter, status int, data interface{}) {
@@ -104,23 +104,31 @@ func responseByJson(w http.ResponseWriter, status int, data interface{}) {
 func (h *sqlHandler) SignUp(w http.ResponseWriter, r *http.Request) {
 	var user User
 	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-		log.Printf("failed to Decode the user: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 	if errorMsg := UserValidate(&user); errorMsg != nil {
 		responseByJson(w, http.StatusBadRequest, errorMsg)
 		return
 	}
-	if errorMsg := checkForUniqueID(h, &user); errorMsg != nil {
+	errorMsg, err := checkForUniqueID(h, &user)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if errorMsg != nil {
 		responseByJson(w, http.StatusConflict, errorMsg)
 		return
 	}
 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
 	if err != nil {
-		log.Fatal(err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 	user.Password = string(hash)
 	if _, err := h.db.Exec("INSERT INTO users(id, name, email, password) VALUES(?,?,?,?)", user.ID, user.Name, user.Email, user.Password); err != nil {
-		log.Printf("failed to insert the users: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 	user.Password = ""
 	responseByJson(w, http.StatusOK, user)
