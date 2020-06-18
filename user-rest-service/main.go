05848805afd947683c7d8b977c2d0d68eeb7b77f
Yusuke Taira <ney07214545@icloud.com>
2020-06-18 16:16:26 +0900

    Update omitempty to json tag of User struct

--- user-rest-service/main.go
+++ user-rest-service/main.go
@@ -16,10 +16,10 @@ import (
 )
 
 type User struct {
-	ID       string `json:"id"       db:"id"       validate:"required,min=1,max=10,excludesall= "`
-	Name     string `json:"name"     db:"name"     validate:"required,min=1,max=50,excludesall= "`
-	Email    string `json:"email"    db:"email"    validate:"required,email,min=5,max=50,excludesall= "`
-	Password string `json:"password" db:"password" validate:"required,min=8,max=50,excludesall= "`
+	ID       string `json:"id"                 db:"id"       validate:"required,min=1,max=10,excludesall= "`
+	Name     string `json:"name"               db:"name"     validate:"required,min=1,max=50,excludesall= "`
+	Email    string `json:"email"              db:"email"    validate:"required,email,min=5,max=50,excludesall= "`
+	Password string `json:"password,omitempty" db:"password" validate:"required,min=8,max=50,excludesall= "`
 }
 
 type ErrorMsg struct {
