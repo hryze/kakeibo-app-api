28b33ec2bb69faa09e9c5de00f59a9465beddf0c
Yusuke Taira <ney07214545@icloud.com>
2020-07-11 02:09:14 +0900

    Modify the BigCategory struct

--- acount-rest-service/domain/model/model.go
+++ acount-rest-service/domain/model/model.go
@@ -1,14 +1,19 @@
 package model
 
+import (
+	"encoding/json"
+	"log"
+)
+
 type CategoriesList struct {
 	CategoriesList []BigCategory `json:"categories_list"`
 }
 
 type BigCategory struct {
-	Type                     string        `json:"type"`
-	ID                       int           `json:"id"   db:"id"`
-	Name                     string        `json:"name" db:"category_name"`
-	AssociatedCategoriesList []interface{} `json:"associated_categories_list"`
+	Type                     string               `json:"type"`
+	ID                       int                  `json:"id"   db:"id"`
+	Name                     string               `json:"name" db:"category_name"`
+	AssociatedCategoriesList []AssociatedCategory `json:"associated_categories_list"`
 }
 
 type MediumCategory struct {
@@ -23,6 +28,26 @@ type CustomCategory struct {
 	ID            int    `json:"id"              db:"id"`
 	Name          string `json:"name"            db:"category_name"`
 	BigCategoryID int    `json:"big_category_id" db:"big_category_id"`
+}
+
+type AssociatedCategory interface {
+	showCategory() string
+}
+
+func (c MediumCategory) showCategory() string {
+	b, err := json.Marshal(c)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(b)
+}
+
+func (c CustomCategory) showCategory() string {
+	b, err := json.Marshal(c)
+	if err != nil {
+		log.Println(err)
+	}
+	return string(b)
 }
 
 func NewCategoriesList(bigCategoriesList []BigCategory) CategoriesList {
