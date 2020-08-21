8deac8a55c6ab871c31df1a631e0bbee79f8c4a0
Yusuke Taira <ney07214545@icloud.com>
2020-08-21 20:59:29 +0900

    Modify acount-rest-service injector

--- acount-rest-service/injector/injector.go
+++ acount-rest-service/injector/injector.go
@@ -27,49 +27,14 @@ func InjectRedis() *infrastructure.RedisHandler {
 	return redisHandler
 }
 
-func InjectAuthRepository() *infrastructure.AuthRepository {
-	redisHandler := InjectRedis()
-	return infrastructure.NewAuthRepository(redisHandler)
-}
-
-func InjectTransactionsRepository() *infrastructure.TransactionsRepository {
-	mySQLHandler := InjectMySQL()
-	return infrastructure.NewTransactionsRepository(mySQLHandler)
-}
-
-func InjectCategoriesRepository() *infrastructure.CategoriesRepository {
-	mySQLHandler := InjectMySQL()
-	return infrastructure.NewCategoriesRepository(mySQLHandler)
-}
-
-func InjectBudgetsRepository() *infrastructure.BudgetsRepository {
-	mySQLHandler := InjectMySQL()
-	return infrastructure.NewBudgetsRepository(mySQLHandler)
-}
-
-func InjectGroupTransactionsRepository() *infrastructure.GroupTransactionsRepository {
-	mySQLHandler := InjectMySQL()
-	return infrastructure.NewGroupTransactionsRepository(mySQLHandler)
-}
-
-func InjectGroupCategoriesRepository() *infrastructure.GroupCategoriesRepository {
-	mySQLHandler := InjectMySQL()
-	return infrastructure.NewGroupCategoriesRepository(mySQLHandler)
-}
-
-func InjectGroupBudgetsRepository() *infrastructure.GroupBudgetsRepository {
-	mySQLHandler := InjectMySQL()
-	return infrastructure.NewGroupBudgetsRepository(mySQLHandler)
-}
-
 func InjectDBHandler() *handler.DBHandler {
 	return &handler.DBHandler{
-		AuthRepo:              InjectAuthRepository(),
-		TransactionsRepo:      InjectTransactionsRepository(),
-		CategoriesRepo:        InjectCategoriesRepository(),
-		BudgetsRepo:           InjectBudgetsRepository(),
-		GroupTransactionsRepo: InjectGroupTransactionsRepository(),
-		GroupCategoriesRepo:   InjectGroupCategoriesRepository(),
-		GroupBudgetsRepo:      InjectGroupBudgetsRepository(),
+		AuthRepo:              infrastructure.NewAuthRepository(InjectRedis()),
+		TransactionsRepo:      infrastructure.NewTransactionsRepository(InjectMySQL()),
+		CategoriesRepo:        infrastructure.NewCategoriesRepository(InjectMySQL()),
+		BudgetsRepo:           infrastructure.NewBudgetsRepository(InjectMySQL()),
+		GroupTransactionsRepo: infrastructure.NewGroupTransactionsRepository(InjectMySQL()),
+		GroupCategoriesRepo:   infrastructure.NewGroupCategoriesRepository(InjectMySQL()),
+		GroupBudgetsRepo:      infrastructure.NewGroupBudgetsRepository(InjectMySQL()),
 	}
 }
