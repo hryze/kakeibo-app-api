6568ffa689096df048bf4c353b644bc106da97e2
Yusuke Taira <ney07214545@icloud.com>
2020-08-25 18:29:00 +0900

    Add test for the PostGroup handler

--- user-rest-service/handler/group_test.go
+++ user-rest-service/handler/group_test.go
@@ -1,8 +1,11 @@
 package handler
 
 import (
+	"database/sql"
+	"net"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/google/uuid"
@@ -14,7 +17,17 @@ import (
 	"github.com/paypay3/kakeibo-app-api/user-rest-service/domain/repository"
 )
 
-type MockGroupRepository struct{ repository.GroupRepository }
+type MockGroupRepository struct {
+	repository.GroupRepository
+}
+
+type MockSqlResult struct {
+	sql.Result
+}
+
+func (r MockSqlResult) LastInsertId() (int64, error) {
+	return 1, nil
+}
 
 func (t MockGroupRepository) GetApprovedGroupList(userID string) ([]model.ApprovedGroup, error) {
 	return []model.ApprovedGroup{
@@ -55,6 +68,21 @@ func (t MockGroupRepository) GetUnapprovedUsersList(unapprovedGroupIDList []inte
 	}, nil
 }
 
+func (t MockGroupRepository) PostGroupAndApprovedUser(group *model.Group, userID string) (sql.Result, error) {
+	return MockSqlResult{}, nil
+}
+
+func (t MockGroupRepository) DeleteGroupAndApprovedUser(groupID int, userID string) error {
+	return nil
+}
+
+func (t MockGroupRepository) GetGroup(groupID int) (*model.Group, error) {
+	return &model.Group{
+		GroupID:   1,
+		GroupName: "group1",
+	}, nil
+}
+
 func TestDBHandler_GetGroupList(t *testing.T) {
 	h := DBHandler{
 		AuthRepo:  MockAuthRepository{},
@@ -78,4 +106,45 @@ func TestDBHandler_GetGroupList(t *testing.T) {
 
 	testutil.AssertResponseHeader(t, res, http.StatusOK)
 	testutil.AssertResponseBody(t, res, "./testdata/group/get_group_list/response.json.golden")
+}
+
+func TestDBHandler_PostGroup(t *testing.T) {
+	postInitGroupStandardBudgetsHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusCreated)
+	})
+
+	listener, err := net.Listen("tcp", "127.0.0.1:8081")
+	if err != nil {
+		t.Fatalf("unexpected error by net.Listen() '%#v'", err)
+	}
+
+	ts := httptest.Server{
+		Listener: listener,
+		Config:   &http.Server{Handler: postInitGroupStandardBudgetsHandler},
+	}
+	ts.Start()
+	defer ts.Close()
+
+	h := DBHandler{
+		AuthRepo:  MockAuthRepository{},
+		GroupRepo: MockGroupRepository{},
+	}
+
+	r := httptest.NewRequest("POST", "/groups", strings.NewReader(testutil.GetJsonFromTestData(t, "./testdata/group/post_group/request.json")))
+	w := httptest.NewRecorder()
+
+	cookie := &http.Cookie{
+		Name:  "session_id",
+		Value: uuid.New().String(),
+	}
+
+	r.AddCookie(cookie)
+
+	h.PostGroup(w, r)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	testutil.AssertResponseHeader(t, res, http.StatusCreated)
+	testutil.AssertResponseBody(t, res, "./testdata/group/post_group/response.json.golden")
 }
