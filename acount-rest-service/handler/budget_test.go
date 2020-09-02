141fb948a7c391f2a1eaa97de8f75fbaba8f5e5a
Yusuke Taira <ney07214545@icloud.com>
2020-09-02 16:53:53 +0900

    Add test for the GetCustomBudgets handler

--- acount-rest-service/handler/budget_test.go
+++ acount-rest-service/handler/budget_test.go
@@ -5,6 +5,9 @@ import (
 	"net/http/httptest"
 	"strings"
 	"testing"
+	"time"
+
+	"github.com/gorilla/mux"
 
 	"github.com/google/uuid"
 	"github.com/paypay3/kakeibo-app-api/acount-rest-service/domain/model"
@@ -48,6 +51,29 @@ func (m MockBudgetsRepository) GetStandardBudgets(userID string) (*model.Standar
 
 func (m MockBudgetsRepository) PutStandardBudgets(standardBudgets *model.StandardBudgets, userID string) error {
 	return nil
+}
+
+func (m MockBudgetsRepository) GetCustomBudgets(yearMonth time.Time, userID string) (*model.CustomBudgets, error) {
+	return &model.CustomBudgets{
+		CustomBudgets: []model.CustomBudgetByCategory{
+			{BigCategoryID: 2, BigCategoryName: "食費", Budget: 30000},
+			{BigCategoryID: 3, BigCategoryName: "日用品", Budget: 5000},
+			{BigCategoryID: 4, BigCategoryName: "趣味・娯楽", Budget: 4500},
+			{BigCategoryID: 5, BigCategoryName: "交際費", Budget: 1000},
+			{BigCategoryID: 6, BigCategoryName: "交通費", Budget: 1000},
+			{BigCategoryID: 7, BigCategoryName: "衣服・美容", Budget: 0},
+			{BigCategoryID: 8, BigCategoryName: "健康・医療", Budget: 4900},
+			{BigCategoryID: 9, BigCategoryName: "通信費", Budget: 4400},
+			{BigCategoryID: 10, BigCategoryName: "教養・教育", Budget: 10000},
+			{BigCategoryID: 11, BigCategoryName: "住宅", Budget: 15000},
+			{BigCategoryID: 12, BigCategoryName: "水道・光熱費", Budget: 3000},
+			{BigCategoryID: 13, BigCategoryName: "自動車", Budget: 0},
+			{BigCategoryID: 14, BigCategoryName: "保険", Budget: 9800},
+			{BigCategoryID: 15, BigCategoryName: "税金・社会保険", Budget: 0},
+			{BigCategoryID: 16, BigCategoryName: "現金・カード", Budget: 0},
+			{BigCategoryID: 17, BigCategoryName: "その他", Budget: 0},
+		},
+	}, nil
 }
 
 func TestDBHandler_PostInitStandardBudgets(t *testing.T) {
@@ -117,4 +143,33 @@ func TestDBHandler_PutStandardBudgets(t *testing.T) {
 
 	testutil.AssertResponseHeader(t, res, http.StatusOK)
 	testutil.AssertResponseBody(t, res, &model.StandardBudgets{}, &model.StandardBudgets{})
+}
+
+func TestDBHandler_GetCustomBudgets(t *testing.T) {
+	h := DBHandler{
+		AuthRepo:    MockAuthRepository{},
+		BudgetsRepo: MockBudgetsRepository{},
+	}
+
+	r := httptest.NewRequest("GET", "/custom-budgets/2020-07", nil)
+	w := httptest.NewRecorder()
+
+	r = mux.SetURLVars(r, map[string]string{
+		"year_month": "2020-07",
+	})
+
+	cookie := &http.Cookie{
+		Name:  "session_id",
+		Value: uuid.New().String(),
+	}
+
+	r.AddCookie(cookie)
+
+	h.GetCustomBudgets(w, r)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	testutil.AssertResponseHeader(t, res, http.StatusOK)
+	testutil.AssertResponseBody(t, res, &model.CustomBudgets{}, &model.CustomBudgets{})
 }
