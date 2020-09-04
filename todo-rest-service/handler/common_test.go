8e9e245b62d65aa4e79bf1d20598f610ee93eec4
Yusuke Taira <ney07214545@icloud.com>
2020-09-04 23:24:56 +0900

    Delete MockSqlResult struct

--- todo-rest-service/handler/common_test.go
+++ todo-rest-service/handler/common_test.go
@@ -1,17 +1,7 @@
 package handler
 
-import "database/sql"
-
 type MockAuthRepository struct{}
-
-type MockSqlResult struct {
-	sql.Result
-}
 
 func (t MockAuthRepository) GetUserID(sessionID string) (string, error) {
 	return "userID1", nil
-}
-
-func (r MockSqlResult) LastInsertId() (int64, error) {
-	return 1, nil
 }
