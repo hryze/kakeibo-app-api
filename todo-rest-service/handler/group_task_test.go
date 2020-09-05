67e532bb6924325ea985b1fcc00f3a9549bee52a
Yusuke Taira <ney07214545@icloud.com>
2020-09-05 22:34:16 +0900

    Add test for the GetGroupTasksList handler

--- todo-rest-service/handler/group_task_test.go
+++ todo-rest-service/handler/group_task_test.go
@@ -82,6 +82,56 @@ func (m MockGroupTasksRepository) PostGroupTasksUser(groupTasksUser model.GroupT
 	return MockSqlResult{}, nil
 }
 
+func (m MockGroupTasksRepository) GetGroupTasksList(groupID int) ([]model.GroupTask, error) {
+	return []model.GroupTask{
+		{
+			ID:               1,
+			BaseDate:         model.NullTime{NullTime: sql.NullTime{Time: time.Date(2020, 9, 5, 0, 0, 0, 0, time.UTC), Valid: true}},
+			CycleType:        model.NullString{NullString: sql.NullString{String: "every", Valid: true}},
+			Cycle:            model.NullInt{Int: 1, Valid: true},
+			TaskName:         "料理",
+			GroupID:          1,
+			GroupTasksUserID: model.NullInt{Int: 2, Valid: true},
+		},
+		{
+			ID:               2,
+			BaseDate:         model.NullTime{NullTime: sql.NullTime{Time: time.Date(2020, 9, 3, 0, 0, 0, 0, time.UTC), Valid: true}},
+			CycleType:        model.NullString{NullString: sql.NullString{String: "every", Valid: true}},
+			Cycle:            model.NullInt{Int: 3, Valid: true},
+			TaskName:         "洗濯",
+			GroupID:          1,
+			GroupTasksUserID: model.NullInt{Int: 1, Valid: true},
+		},
+		{
+			ID:               3,
+			BaseDate:         model.NullTime{NullTime: sql.NullTime{Time: time.Date(2020, 9, 2, 0, 0, 0, 0, time.UTC), Valid: true}},
+			CycleType:        model.NullString{NullString: sql.NullString{String: "every", Valid: true}},
+			Cycle:            model.NullInt{Int: 7, Valid: true},
+			TaskName:         "トイレ掃除",
+			GroupID:          1,
+			GroupTasksUserID: model.NullInt{Int: 4, Valid: true},
+		},
+		{
+			ID:               4,
+			BaseDate:         model.NullTime{NullTime: sql.NullTime{Time: time.Date(0, 0, 0, 0, 0, 0, 0, time.UTC), Valid: false}},
+			CycleType:        model.NullString{NullString: sql.NullString{String: "", Valid: false}},
+			Cycle:            model.NullInt{Int: 0, Valid: false},
+			TaskName:         "台所掃除",
+			GroupID:          1,
+			GroupTasksUserID: model.NullInt{Int: 0, Valid: false},
+		},
+		{
+			ID:               5,
+			BaseDate:         model.NullTime{NullTime: sql.NullTime{Time: time.Date(2020, 8, 31, 0, 0, 0, 0, time.UTC), Valid: true}},
+			CycleType:        model.NullString{NullString: sql.NullString{String: "consecutive", Valid: true}},
+			Cycle:            model.NullInt{Int: 7, Valid: true},
+			TaskName:         "風呂掃除",
+			GroupID:          1,
+			GroupTasksUserID: model.NullInt{Int: 3, Valid: true},
+		},
+	}, nil
+}
+
 func TestDBHandler_GetGroupTasksListForEachUser(t *testing.T) {
 	tearDown := testutil.SetUpMockServer(t)
 	defer tearDown()
@@ -144,4 +194,36 @@ func TestDBHandler_PostGroupTasksUser(t *testing.T) {
 
 	testutil.AssertResponseHeader(t, res, http.StatusCreated)
 	testutil.AssertResponseBody(t, res, &model.GroupTasksUser{}, &model.GroupTasksUser{})
+}
+
+func TestDBHandler_GetGroupTasksList(t *testing.T) {
+	tearDown := testutil.SetUpMockServer(t)
+	defer tearDown()
+
+	h := DBHandler{
+		AuthRepo:       MockAuthRepository{},
+		GroupTasksRepo: MockGroupTasksRepository{},
+	}
+
+	r := httptest.NewRequest("GET", "/groups/1/tasks", nil)
+	w := httptest.NewRecorder()
+
+	r = mux.SetURLVars(r, map[string]string{
+		"group_id": "1",
+	})
+
+	cookie := &http.Cookie{
+		Name:  "session_id",
+		Value: uuid.New().String(),
+	}
+
+	r.AddCookie(cookie)
+
+	h.GetGroupTasksList(w, r)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	testutil.AssertResponseHeader(t, res, http.StatusOK)
+	testutil.AssertResponseBody(t, res, &model.GroupTasksList{}, &model.GroupTasksList{})
 }
