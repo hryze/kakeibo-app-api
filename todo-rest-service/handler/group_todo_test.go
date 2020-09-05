ccd51767d5fab64f966fddaf30171929c75df2c0
Yusuke Taira <ney07214545@icloud.com>
2020-09-05 14:23:32 +0900

    Add test for the GetMonthlyGroupTodoList handler

--- todo-rest-service/handler/group_todo_test.go
+++ todo-rest-service/handler/group_todo_test.go
@@ -30,6 +30,22 @@ func (m MockGroupTodoRepository) GetDailyDueGroupTodoList(date time.Time, groupI
 	}, nil
 }
 
+func (m MockGroupTodoRepository) GetMonthlyImplementationGroupTodoList(firstDay time.Time, lastDay time.Time, groupID int) ([]model.GroupTodo, error) {
+	return []model.GroupTodo{
+		{ID: 1, PostedDate: time.Date(2020, 9, 5, 1, 29, 8, 0, time.UTC), ImplementationDate: model.Date{Time: time.Date(2020, 7, 5, 0, 0, 0, 0, time.UTC)}, DueDate: model.Date{Time: time.Date(2020, 7, 5, 0, 0, 0, 0, time.UTC)}, TodoContent: "今月の予算を立てる", CompleteFlag: true, UserID: "userID1"},
+		{ID: 2, PostedDate: time.Date(2020, 9, 5, 1, 29, 8, 0, time.UTC), ImplementationDate: model.Date{Time: time.Date(2020, 7, 9, 0, 0, 0, 0, time.UTC)}, DueDate: model.Date{Time: time.Date(2020, 7, 10, 0, 0, 0, 0, time.UTC)}, TodoContent: "コストコ鶏肉セール 5パック購入", CompleteFlag: true, UserID: "userID2"},
+		{ID: 3, PostedDate: time.Date(2020, 9, 5, 1, 29, 8, 0, time.UTC), ImplementationDate: model.Date{Time: time.Date(2020, 7, 10, 0, 0, 0, 0, time.UTC)}, DueDate: model.Date{Time: time.Date(2020, 7, 12, 0, 0, 0, 0, time.UTC)}, TodoContent: "醤油購入", CompleteFlag: false, UserID: "userID1"},
+	}, nil
+}
+
+func (m MockGroupTodoRepository) GetMonthlyDueGroupTodoList(firstDay time.Time, lastDay time.Time, groupID int) ([]model.GroupTodo, error) {
+	return []model.GroupTodo{
+		{ID: 1, PostedDate: time.Date(2020, 9, 5, 1, 29, 8, 0, time.UTC), ImplementationDate: model.Date{Time: time.Date(2020, 7, 5, 0, 0, 0, 0, time.UTC)}, DueDate: model.Date{Time: time.Date(2020, 7, 5, 0, 0, 0, 0, time.UTC)}, TodoContent: "今月の予算を立てる", CompleteFlag: true, UserID: "userID1"},
+		{ID: 2, PostedDate: time.Date(2020, 9, 5, 1, 29, 8, 0, time.UTC), ImplementationDate: model.Date{Time: time.Date(2020, 7, 9, 0, 0, 0, 0, time.UTC)}, DueDate: model.Date{Time: time.Date(2020, 7, 10, 0, 0, 0, 0, time.UTC)}, TodoContent: "コストコ鶏肉セール 5パック購入", CompleteFlag: true, UserID: "userID2"},
+		{ID: 3, PostedDate: time.Date(2020, 9, 5, 1, 29, 8, 0, time.UTC), ImplementationDate: model.Date{Time: time.Date(2020, 7, 10, 0, 0, 0, 0, time.UTC)}, DueDate: model.Date{Time: time.Date(2020, 7, 12, 0, 0, 0, 0, time.UTC)}, TodoContent: "醤油購入", CompleteFlag: false, UserID: "userID1"},
+	}, nil
+}
+
 func TestDBHandler_GetDailyGroupTodoList(t *testing.T) {
 	tearDown := testutil.SetUpMockServer(t)
 	defer tearDown()
@@ -55,6 +71,39 @@ func TestDBHandler_GetDailyGroupTodoList(t *testing.T) {
 	r.AddCookie(cookie)
 
 	h.GetDailyGroupTodoList(w, r)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	testutil.AssertResponseHeader(t, res, http.StatusOK)
+	testutil.AssertResponseBody(t, res, &model.GroupTodoList{}, &model.GroupTodoList{})
+}
+
+func TestDBHandler_GetMonthlyGroupTodoList(t *testing.T) {
+	tearDown := testutil.SetUpMockServer(t)
+	defer tearDown()
+
+	h := DBHandler{
+		AuthRepo:      MockAuthRepository{},
+		GroupTodoRepo: MockGroupTodoRepository{},
+	}
+
+	r := httptest.NewRequest("GET", "/groups/1/todo-list/2020-07", nil)
+	w := httptest.NewRecorder()
+
+	r = mux.SetURLVars(r, map[string]string{
+		"group_id":   "1",
+		"year_month": "2020-07",
+	})
+
+	cookie := &http.Cookie{
+		Name:  "session_id",
+		Value: uuid.New().String(),
+	}
+
+	r.AddCookie(cookie)
+
+	h.GetMonthlyGroupTodoList(w, r)
 
 	res := w.Result()
 	defer res.Body.Close()
