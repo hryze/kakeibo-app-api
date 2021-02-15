e2607b81d769e0e622dee95e3def735fa408c426
Yusuke Taira <ney07214545@icloud.com>
2021-02-15 12:54:25 +0900

    Add Makefile

--- user-rest-service/config/env.go
+++ user-rest-service/config/env.go
@@ -1,29 +1,19 @@
 package config
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"time"
 
-	"github.com/joho/godotenv"
 	"github.com/kelseyhightower/envconfig"
 )
 
 var Env ENV
 
 func init() {
-	isLocal := flag.Bool("local", false, "Please specify -local flag")
-	flag.Parse()
+	env := os.Getenv("GO_ENV")
 
-	if *isLocal {
-		if err := godotenv.Load("../../development.env"); err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(1)
-		}
-	}
-
-	if err := envconfig.Process("", &Env); err != nil {
+	if err := envconfig.Process(env, &Env); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
@@ -38,33 +28,33 @@ type ENV struct {
 	AccountApi accountApi
 }
 
-type server struct {
+type Server struct {
 	Port int `envconfig:"SERVER_PORT" required:"true"`
 }
 
-type cors struct {
+type Cors struct {
 	AllowedOrigins []string `envconfig:"CORS_ALLOWED_ORIGINS" required:"true"`
 }
 
-type cookie struct {
+type Cookie struct {
 	Domain string `envconfig:"COOKIE_DOMAIN" required:"true"`
 }
 
-type mysql struct {
+type MySQL struct {
 	Dsn             string        `envconfig:"MYSQL_DSN"               required:"true"`
 	MaxConn         int           `envconfig:"MYSQL_MAX_CONN"          default:"25"`
 	MaxIdleConn     int           `envconfig:"MYSQL_MAX_IDLE"          default:"25"`
 	MaxConnLifetime time.Duration `envconfig:"MYSQL_MAX_CONN_LIFETIME" default:"300s"`
 }
 
-type redis struct {
+type Redis struct {
 	Dsn             string        `envconfig:"REDIS_DSN"               required:"true"`
 	MaxConn         int           `envconfig:"REDIS_MAX_CONN"          default:"25"`
 	MaxIdleConn     int           `envconfig:"REDIS_MAX_IDLE"          default:"25"`
 	MaxConnLifetime time.Duration `envconfig:"REDIS_MAX_CONN_LIFETIME" default:"300s"`
 }
 
-type accountApi struct {
+type AccountApi struct {
 	Host string `envconfig:"ACCOUNT_HOST" required:"true"`
 	Port int    `envconfig:"ACCOUNT_PORT" required:"true"`
 }
